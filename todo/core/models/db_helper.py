d7b37d80a6d3789cfea7e1afa73f0f0ae5c1831f
ivan-karaberov <karaberov.ivan@yandex.ru>
2024-02-01 17:38:21 +0300

    feat: create db helper

---
+++ todo/core/models/db_helper.py
@@ -0,0 +1,20 @@
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, \
+                                    create_async_engine
+
+from core.config import settings
+
+
+class DatabaseHelper:
+    def __init__(self, url: str, echo: bool = False):
+        self.engine = create_async_engine(url=url, echo=echo)
+        self.session_factory = async_sessionmaker(
+            bind=self.engine, autoflush=False, autocommit=False, expire_on_commit=False
+        )
+
+    async def session_dependency(self) -> AsyncSession:
+        async with self.session_factory() as session:
+            yield session
+            await session.close()
+
+
+db_helper = DatabaseHelper(url=settings.url, echo=settings.echo)
