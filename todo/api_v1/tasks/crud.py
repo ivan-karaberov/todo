f87943d5357843674a03064680acfbf8db3f2152
ivan-karaberov <karaberov.ivan@yandex.ru>
2024-02-02 18:18:20 +0300

    feat: add function for get tasks for db

--- todo/api_v1/tasks/crud.py
+++ todo/api_v1/tasks/crud.py
@@ -1,3 +1,4 @@
+from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from core.models import Task
@@ -11,3 +12,16 @@ async def create_task(
     session.add(task)
     await session.commit()
     return task
+
+
+async def get_tasks_list(
+    session: AsyncSession,
+    user_id: int
+) -> list[Task]:
+    stmt = (
+        select(Task)
+        .where(Task.user_id == user_id)
+        .order_by(Task.id.desc())
+    )
+    tasks = await session.scalars(stmt)
+    return tasks.all()
