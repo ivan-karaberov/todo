a45f6743fcad010a9e84b1bafe6916233ba18f3b
ivan-karaberov <karaberov.ivan@yandex.ru>
2024-02-03 22:26:22 +0300

    feat: create view function for create task

--- todo/api_v1/tasks/views.py
+++ todo/api_v1/tasks/views.py
@@ -1,11 +1,23 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from . import crud
+from core.models import db_helper
 
 router = APIRouter()
 
 
 @router.post("/")
-def create_task():
-    pass
+async def create_task(
+    user_id: int,
+    task_title: str,
+    session: AsyncSession = Depends(db_helper.session_dependency)
+):
+    return await crud.create_task(
+        session=session, 
+        user_id=user_id,
+        task_title=task_title
+    )
 
 
 @router.get("/")
